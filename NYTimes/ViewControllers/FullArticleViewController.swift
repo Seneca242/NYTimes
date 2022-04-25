fac752777ebffbb43aca62b3602436f9e4b4df5b
Seneca <flabbergasted242@gmail.com>
2022-04-25 11:00:17 +0300

    made full article screen

--- NYTimes/ViewControllers/FullArticleViewController.swift
+++ NYTimes/ViewControllers/FullArticleViewController.swift
@@ -6,20 +6,45 @@
 //
 
 import UIKit
+import WebKit
 
-class FullArticleViewController: UIViewController {
+class FullArticleViewController: UIViewController, WKUIDelegate {
 
     @IBOutlet var articleLabel: UILabel!
+    var webView: WKWebView!
     
-//    var article: ArticleDetails!
     var fullArticleUrl: String!
+    
+    override func loadView() {
+        overrideView()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        articleLabel.text = article.summary_short
+        loadWebView(fullArticleUrl)
+    }
+}
+
+extension FullArticleViewController {
+    
+    private func overrideView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    private func loadWebView(_ string: String?) {
+        guard let fullArticleUrl = fullArticleUrl else { return }
+        
+        let myURL = URL(string: fullArticleUrl)
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
     
     private func fetchFullArticle() {
+        guard let fullArticleUrl = fullArticleUrl else { return }
+        
         NetworkManager.shared.fetchFullArticle(from: fullArticleUrl) { [weak self] results in
             switch results {
             case .success(let fullArtilce):
